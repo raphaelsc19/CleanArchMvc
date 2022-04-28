e6c7380d6f2bdc29538d342b2e04517ffb3952a4
Raphael Soares <raphael_sc_@hotmail.com>
2022-04-28 15:26:59 -0300

    Commit para alteração de teste para estudos (Bons Estudos!)

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -12,7 +12,9 @@ namespace CleanArchMvc.Infra.Data.Context
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
-        { }
+        {
+        
+        }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
