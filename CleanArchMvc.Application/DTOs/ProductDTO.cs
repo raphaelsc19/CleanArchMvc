ff7050c2bf700d0f0b3f3c689e9029c37c6f68a9
Raphael Soares <raphael_sc_@hotmail.com>
2022-05-04 19:29:01 -0300

    Mapeando as entidades de dominios com DTOs (Data Transfer Objects) utilizando AutoMapper

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "This Description is Required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "This Price is Required")]
+        [Column(TypeName="decimal(18,2)")]
+        [DisplayFormat(DataFormatString="{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+        
+        [Required(ErrorMessage = "This Stock is Required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; set; }
+        
+        public int CategoryId { get; set; }
+
+        [DisplayName("Categories")]
+        public Category Category { get; set; }
+    }
+}
